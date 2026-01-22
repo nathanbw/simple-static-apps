230735a2555ea56c6ee4892c83b9cada3aefef47
Nathan Wallace <dev@nathanbw.com>
2026-01-21 23:27:07 -0500

    GoldStarCalendar: Make it a PWA!
    Junie Prompt:
    
    What would we need to do in order to make this page a progressive web app? I've
    heard that we would have to add a manifest file describing the app. If it only
    involves minimal changes, go ahead and implement it. Otherwise, just list what
    steps we will need to take.

---
+++ GoldStarCalendar/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'goldstar-calendar-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+// Install event - cache assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(ASSETS);
+      })
+  );
+});
+
+// Activate event - cleanup old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch event - serve from cache or network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
